e173452be252b7361183d37e67ebc4da10dc9f31
Shubham <shubhamsingh9816@gmail.com>
2024-01-03 22:09:55 +0530

    modules added

--- src/db/dbConnect.js
+++ src/db/dbConnect.js
@@ -4,11 +4,11 @@ import { DB_NAME } from "../constants.js";
 const connectDB = async ()=> {
     try {
         const DBconnection = await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`);
-        console.log(`Successfully connected to MongoDB !! DB HOST:${DBconnection}`)
+        console.log(`Successfully connected to MongoDB !! DB HOST:${DBconnection.connection.host}`);
         
     } catch (error) {
         console.error("MongoDB connection failed ", error);
-        throw error;
+        process.exit(1);
         
     }
 
--- src/index.js
+++ src/index.js
@@ -1,8 +1,17 @@
 import dotenv from "dotenv";
 import connectDB from "./db/dbConnect.js";
+import { app } from "./app.js";
 
 dotenv.config({
     path : './env'
 })
 
-connectDB();
+connectDB()
+.then(() => {
+    app.listen(process.env.PORT || 3000, ()=>{
+        console.log(`Server is running successfully at PORT: ${process.env.PORT}`);
+    })
+})
+.catch((error) => {
+    console.log("Mongo db connection Failed !!!!", error);
+})
